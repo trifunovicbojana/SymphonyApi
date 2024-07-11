04479ea46aa6b1713000b7f6539f30175d598d49
btrifunovic <bojana.trifunovic@sixsentix.com>
2024-07-11 14:25:38 +0200

    Feature file updated

--- src/test/java/runners/PartIIRunner.java
+++ src/test/java/runners/PartIIRunner.java
@@ -9,5 +9,5 @@ import io.cucumber.testng.CucumberOptions;
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 
 )
-public class TaskIIRunner extends AbstractTestNGCucumberTests {
+public class PartIIRunner extends AbstractTestNGCucumberTests {
 }
