85b175084b1fa05979c155857e3b4b8dad23a8ff
btrifunovic <bojana.trifunovic@sixsentix.com>
2024-07-11 14:32:11 +0200

    Endpoints file updated

--- src/main/java/helpers/Endpoints.java
+++ src/main/java/helpers/Endpoints.java
@@ -2,10 +2,10 @@ package helpers;
 
 public interface Endpoints {
 String TESTING_ENVIRONMENT =  System.getProperty("environment", "https://randomlyapi.symphony.is/api");
-String SIGNUP=TESTING_ENVIRONMENT+"/auth/signup/";
-String USERS=TESTING_ENVIRONMENT+"/users/";
-String LOGIN=TESTING_ENVIRONMENT+"/auth/login/";
-String POST=TESTING_ENVIRONMENT+"/posts/";
-String POST_COMMENTS=TESTING_ENVIRONMENT+"/post-comments/";
+String SIGNUP="/auth/signup/";
+String USERS="/users/";
+String LOGIN="/auth/login/";
+String POST="/posts/";
+String POST_COMMENTS="/post-comments/";
 
 }
